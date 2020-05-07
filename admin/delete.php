b104eb0307f77cbe06dba912475eb7b29a74446a
wyeg732 <62007537+wyeg732@users.noreply.github.com>
2020-05-07 15:02:56 +0300

    Update delete.php

--- admin/delete.php
+++ admin/delete.php
@@ -2,8 +2,14 @@
 session_start();
 
 $connect = mysqli_connect("127.0.0.1", "root", "Starkova24", "vhelp"); 
-$query = mysqli_query($connect, 
+
+if (isset($_POST['empty_gig'])) {
+	mysqli_query($connect, 
+  "DELETE FROM gigs WHERE id = '".$_POST["gigs_id"]."' ");
+}else{
+	mysqli_query($connect, 
   "DELETE FROM apps WHERE users_id = '".$_POST["users_id"]."' AND gigs_id = '".$_POST["gigs_id"]."' ");
+}
 
 header('location: http://localhost/vhelp/admin/index.php');
 
